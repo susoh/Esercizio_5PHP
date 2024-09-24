27193729eab603bb6e57eb5294cb269fd525ee20
Davide Susini <susini.davide@itismeucci.com>
2024-09-24 12:06:35 +0200

    esercizio finito

---
+++ Esercizio_5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $num = 5;
+    if ($num % 2 == 0) {
+        echo "<p>Il numero $num è pari</p>";
+    }else{
+        echo "<p>Il numero $num è dispari</p>";
+    }
+    ?>
+    
+</body>
+</html>
